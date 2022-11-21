83debf5e864add297bb38a6f034591b4b996dc8b
litarne <705290832@qq.com>
2022-11-21 16:22:18 +0800

    11.21

--- theta/deploy-mainchain.sh
+++ theta/deploy-mainchain.sh
@@ -23,7 +23,7 @@ elif [ "$1" == "init" ]; then
   tmux send -t $tmux_name "cd docker-lab1" Enter
   #tmux send -t $tmux_name "nohup ./earthd start --home=./workspace/earth/validator${i} > output 2>&1 & " Enter
 elif [ "$1" == "start" ]; then
-  tmux send -t $tmux_name "ps -ef | grep theta| grep -v grep | awk '{print $2}' | xargs kill -9" Enter
+  #tmux send -t $tmux_name "ps -ef | grep theta| grep -v grep | awk '{print $2}' | xargs kill -9" Enter
   tmux send -t $tmux_name "cd ~/docker-lab1/theta" Enter
   tmux send -t $tmux_name "nohup ./theta-eth-rpc-adaptor start --config=./eth_rpc_adaptor  > output 2>&1 &  " Enter
   tmux send -t $tmux_name "nohup ./theta start --config=./lab34/node${val}/ --password=qwertyuiop   > ./output.log 2>&1 & " Enter
@@ -37,7 +37,7 @@ elif [ "$1" == "clean" ]; then
   tmux send -t $tmux_name "cd ~" Enter
   tmux send -t $tmux_name "rm -rf docker-lab1" Enter
 elif [ "$1" == "stop" ]; then
-  tmux send -t $tmux_name "ps -ef | grep theta| grep -v grep | awk '{print $2}' | xargs kill -9" Enter
+  tmux send -t $tmux_name " ps -ef | grep theta | grep -v grep | awk '{print $2}' | xargs kill -9" Enter
 fi
 
   echo "start node${val}!"
